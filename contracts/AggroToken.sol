935b3639426d540a7ed1e7e8340a9e70f0d3b73f
SevenSwen <sevenswen@gmail.com>
2021-05-30 21:49:16 +0700

    add stuped token

--- contracts/AggroToken.sol
+++ contracts/AggroToken.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AggroToken {}
+import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract AggroToken is ERC20 {
+    constructor() ERC20('Maximum AGGRessive token', 'AGGRO') {
+        uint256 totalSupply = 987654321000 * 10**13;
+        _mint(msg.sender, totalSupply);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return 13;
+    }
+}
